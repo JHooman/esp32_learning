22a7a5211e87dee719600a57bf32cd64d61ed6c0
Aleksander Szczepocki <olek.szczepocki@gmail.com>
2023-12-19 13:03:33 +0100

    Add TX to RX usart communication

---
+++ 23_12_05_usart_TX_to_RX/uart_echo/main/uart_echo_example_main.c
@@ -0,0 +1,111 @@
+/* UART Echo Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_timer.h"
+#include "esp_log.h"
+
+/**
+ * This is an example which echos any data it receives on configured UART back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: configured UART
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below (See Kconfig)
+ */
+
+#define ECHO_TEST_TXD (CONFIG_EXAMPLE_UART_TXD)
+#define ECHO_TEST_RXD (CONFIG_EXAMPLE_UART_RXD)
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM      (CONFIG_EXAMPLE_UART_PORT_NUM)
+#define ECHO_UART_BAUD_RATE     (CONFIG_EXAMPLE_UART_BAUD_RATE)
+#define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
+
+static const char *TAG = "UART TEST";
+
+#define BUF_SIZE (1024)
+static void uart_read()
+{
+
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    while(1){
+        // Read data from the UART
+        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        // Write data back to the UART
+        uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
+        if (len) {
+            data[len] = '\0';
+            ESP_LOGI(TAG, "Recv str: %s", (char *) data);
+            //ESP_LOG_BUFFER_HEXDUMP(TAG,(char *) data,25,ESP_LOG_INFO);
+        }
+        
+    }
+}
+
+void uart_conf()
+{
+    uart_config_t uart_config = {
+        .baud_rate = ECHO_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    int intr_alloc_flags = 0;
+
+
+
+    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+    xTaskCreate(uart_read, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+}
+
+void uart_write()
+{
+    char data[25];
+    sprintf(data, "UART communication test\n");
+    uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, sizeof(data));
+    ESP_LOGI("write", "usart write");
+    
+
+}
+void timer_conf()
+{
+        const esp_timer_create_args_t periodic_timer_args = {
+            .callback = &uart_write,
+            /* name is optional, but may help identify the timer when debugging */
+            .name = "periodic"
+    };
+
+    esp_timer_handle_t periodic_timer;
+    esp_timer_create(&periodic_timer_args, &periodic_timer);
+    esp_timer_start_periodic(periodic_timer, 1000000);
+}
+void app_main(void)
+{
+    timer_conf();
+    uart_conf();
+    
+    while(1){
+        vTaskDelay(1);
+        
+    }
+}
